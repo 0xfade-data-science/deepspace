fb18f089c756952dd5f93e2d6274d82bda772bea
Fay <26164130+0xfade-data-science@users.noreply.github.com>
2024-05-04 07:14:23 +0200

    Added super monads as a result of composition of unitary monads (SanityCheck, Overview)

--- transformers/overview/Overview.py
+++ transformers/overview/Overview.py
@@ -1,11 +1,27 @@
 
 from deepspace.transformers.Transformer import Transformer
+from deepspace.transformers.overview.Shape import Shape
+from deepspace.transformers.overview.Info import Info
+from deepspace.transformers.overview.HeadTail import HeadTail
+from deepspace.transformers.overview.Describe import DescribeNumerical, DescribeCategorical
+
+from deepspace.transformers.chain.Milestone import Milestone
 from deepspace.DataSpace import DataSpace
 
 class Overview(Transformer):
     ''''''
     def __init__(self):
         Transformer.__init__(self)
+    def transform(self, ds:DataSpace):
+            self.separator(caller=str(self))
+            _ = Milestone(ds) >> Shape() >> HeadTail() >> Info() >> DescribeNumerical() >> DescribeCategorical()
+            return _.ds
+    
+class OverviewOLD(Transformer):
+    ''''''
+    def __init__(self, _):
+        Transformer.__init__(self)
+        self._ = _
     def transform(self, ds: DataSpace):
         self.separator(caller=str(self))
         df = ds.data
